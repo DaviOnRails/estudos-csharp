b5e69be60faa6b5ad19b2ebc71c12ec8811cc94a
DaviRamos <davionrails@gmail.com>
2022-10-01 16:01:54 -0300

    backup

---
+++ calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Calculator
+{
+  class Program
+
+  {
+    static void Main(string[] args)
+    {
+      Soma();
+    }
+
+    static void Soma()
+    {
+      //primeiro valor
+      Console.Clear();
+      Console.WriteLine("primeiro valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      //segundo valor
+      Console.WriteLine("segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine(" ");
+      float resultado = v1 + v2;
+      Console.WriteLine("O resultado da soma é:" + resultado);
+
+    }
+
+    static void subtracao()
+    {
+      //primeiro valor
+      Console.Clear();
+
+      Console.WriteLine("primeiro valor:");
+      float v1 = float.Parse(Console.ReadLine());
+    }
+
+  }
+}
