911b128f9a225c68acd5789b9e12fb528536c627
DaviRamos <davionrails@gmail.com>
2022-10-03 10:04:38 -0300

    start

--- cronometro/Program.cs
+++ cronometro/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+namespace cronometro
+{
+    class program{
+        static void main(string [] args){
+                Start();
+        }
+
+        static void Start(){
+           int time = 10;
+           int currentTime = 10;
+
+
+           while(currentTime != time){
+                 Console.Clear();
+                 currentTime ++;
+                 Console.WriteLine(currentTime);
+                 Thread.Sleep(1000);
+           }
+        }
+
+    }
+}
