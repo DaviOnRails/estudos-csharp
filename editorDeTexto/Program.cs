376533167852f79a8c7cc8fe326d9b6a4d2a0bb9
DaviRamos <davionrails@gmail.com>
2022-10-04 10:15:53 -0300

    editar texto

--- editorDeTexto/Program.cs
+++ editorDeTexto/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Editor{
+
+class Program {
+    static void Main (string[] args){
+            Menu();
+            Editar();
+    }
+
+   static void Menu(){
+    
+    Console.Clear();
+    Console.WritLine("o que você deseja fazer");
+    Console.WritLine("1 - Abrir arquivo");
+    Console.WritLine("2 - Criar novo arqivo");
+    Console.WritLine("0 -  Sair");
+    short option = short.Parse(Console.ReadLine());
+
+    switch(option){
+        case 0:System.Environment.Exit(0);break;
+        case 1:Abrir(); break;
+        case 2:Editar(); break;
+        default: Menu(); break;
+    }
+   }
+   static void Abrir(){
+
+    }
+   static void Editar(){
+           Console.Clear();
+           Console.WriteLine("digite seu texto (esc para sair");
+           Console.WriteLine("----------------");
+           string text = "";
+                 
+            do{
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+            }     
+           while(console.ReadKey().key != ConsoleKey.Escape);
+                    
+           Console.Write(text);
+    }
+   
+}
+}
