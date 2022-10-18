ec8210ae6900f7f0b971c5fb8c677da5d29ec5aa
DaviRamos <davionrails@gmail.com>
2022-10-18 09:58:36 -0300

    salvar arquivo

--- editorDeTexto/Program.cs
+++ editorDeTexto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace EditorDeTexto{
 
@@ -39,17 +40,19 @@ class Program {
             }     
            while(Console.ReadKey().Key != ConsoleKey.Escape);
                     
-           Console.Write(text);
-    }
-   
+           Salvar(text);
+    } 
    static void Salvar(string text){
      Console.Clear();
      Console.WriteLine("Qual caminho para salvar o arquivo");
      var path = Console.ReadLine();
 
-     using(var file = new StreamWriter()){
-
+     using(var file = new StreamWriter(path)){
+        file.Write(text);
      }
+     Console.WriteLine($"Arquivo {path} salvo com sucesso");
+     Console.ReadLine();
+     Menu();
    }
 }
 }
