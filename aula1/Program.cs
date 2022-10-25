ebc4ebc2ade0588fd057fe10bc26753b11b2dacf
DaviRamos <davionrails@gmail.com>
2022-10-25 10:31:37 -0300

    finalizacao de strings

--- aula1/Program.cs
+++ aula1/Program.cs
@@ -7,11 +7,8 @@ namespace aula1
   {
     static void Main(string[] args)
     {
-       var id = Guid.NewGuid();
-       id.ToString();
-
-       id = new Guid("");
-       Console.WriteLine(id);
+      var text = "Testando as strings";
+       Console.WriteLine(text.Equals(""));
     }
   }
 }
