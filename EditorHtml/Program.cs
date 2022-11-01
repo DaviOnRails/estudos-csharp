d2137d339d7698d928924234f0ec694a9a361eef
DaviRamos <davionrails@gmail.com>
2022-11-01 10:30:57 -0300

    start Editor-HTML

---
+++ EditorHtml/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace aula1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var text = "Testando as strings";
+       Console.WriteLine(text.Equals(""));
+    }
+  }
+}
