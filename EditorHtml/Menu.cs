547a6ac95cbd277151586c303d6e6b51b2fcd186
DaviRamos <davionrails@gmail.com>
2022-11-01 10:43:33 -0300

    Menu-do-EditorHtml

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EditoHtml{
+      public static class Menu{
+        public static void Show(){
+           Console.Clear();
+           Console.BackgroundColor = ConsoleColor.black;
+        }
+
+      }
+
+}
--- EditorHtml/Program.cs
+++ EditorHtml/Program.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 
 
-namespace aula1
+namespace EditoHtml
 {
   class Program
   {
     static void Main(string[] args)
     {
-      var text = "Testando as strings";
-       Console.WriteLine(text.Equals(""));
+     
     }
   }
 }
