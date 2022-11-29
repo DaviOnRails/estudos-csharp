d30ebb61310f0cc51c5f2681a9b6c2f312d55454
DaviRamos <davionrails@gmail.com>
2022-11-29 14:08:21 -0300

    start editor

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -6,10 +6,29 @@ namespace EditoHtml{
            Console.Clear();
            Console.BackgroundColor = ConsoleColor.Blue;
            Console.ForegroundColor = ConsoleColor.Black;
+
+           DrawScreen();
         }
          
-        public static void DrawScreen(){
-          
+    public static void DrawScreen(){
+        Console.Write("+");
+
+    for(int i= 0; i <=30;i ++){
+        Console.Write("-");
+    }
+        
+        Console.Write("\n");
+
+        for(int lines = 0;lines <= 10;lines ++){
+                Console.Write("|");
+
+         for(int i = 0;i <= 30;i ++){
+                Console.Write(" ");
+                Console.Write("|");
+        }
+        }
+         Console.Write("\n");
+    
         }
 
       }
