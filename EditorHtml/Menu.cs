8961da63ce9e75e2efd0c3bb024431001c3644fe
DaviRamos <davionrails@gmail.com>
2022-11-29 15:04:11 -0300

    hard

--- EditorHtml/Menu.cs
+++ EditorHtml/Menu.cs
@@ -8,41 +8,22 @@ namespace EditoHtml{
            Console.ForegroundColor = ConsoleColor.Black;
 
            DrawScreen();
+           WriteOptions();
         }
          
     public static void DrawScreen(){
-        Console.Write("+");
-    for(int i= 0; i<=30;i ++){
-        Console.Write("-");
-     }
-
-        Console.Write("+");     
-        Console.Write("\n");
-
-
-        for(int lines = 0;lines <= 10;lines ++){
-                Console.Write("|");
-
-         for(int i = 0;i <= 30;i ++){
-                Console.Write(" ");
-        }
-                Console.Write("|");
-                Console.Write("\n");
+           Console.Write("+");
+           for(int i = 0; i <= 30; i++){
+            Console.Write("-");
+           }
+                 
         }
 
-         Console.Write("+");
-        for(int i= 0; i<=30;i ++){
-        Console.Write("-");
-       }
+    public static void WriteOptions(){
+           //Console.SetCursorPosition(3, 2);
+           //Console.WriteLine("editor");
+    }
 
-        Console.Write("+");     
-        Console.Write("\n");
-
-
-    
-        }
-
-    public static void WriteOptions(){}
       }
 
 }
